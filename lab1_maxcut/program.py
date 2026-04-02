3f68f714451d4fb5ff0c7ed8081f81e82481aa87
Axel-PC <axel.bergenfeldt@gmail.com>
2026-04-02 02:55:40 +0200

    almost done

---
+++ lab1_maxcut/program.py
@@ -0,0 +1,62 @@
+# program.py
+import random
+
+def get_input() :
+    n_m = [int(i) for i in input().split()]
+    n = n_m[0]
+    m = n_m[1]
+    weight = {}
+    connection = {}
+    for k in range(m) :
+        i_j_w = [int(i) for i in input().split()]
+        i = i_j_w[0]
+        j = i_j_w[1]
+        w = i_j_w[2]
+        weight[(i,j)] = w
+        weight[(j,i)] = w
+        if i not in connection :
+            connection[i] = set()
+        connection[i].add(j)
+        if j not in connection :
+            connection[j] = set()
+        connection[j].add(i)
+    return (n,m,weight,connection)
+
+def alg_R(n) :
+    A_B = (list([0]*(n+1)),list([0]*(n+1)))
+    for i in range(n) :
+        A_B[round(random.random())][i+1] = 1
+    return A_B
+
+def alg_S(n,m,weight,connection,A_B) :
+    i = 1
+    while i <= n :
+        value = 0
+        for j in connection[i] :
+            value += weight[(i,j)] * (1 if A_B[1][j] == 1 else -1) # We assume node i is in B
+        in_B = A_B[1][i] == 1 
+        value *= (1 if in_B else -1)    # Here we correct if node i is in A
+        if value > 0 :  # If True, we swap
+            A_B[0][i] = (A_B[0][i] + 1) % 2
+            A_B[1][i] = (A_B[1][i] + 1) % 2
+            i = 1
+            continue
+        i += 1
+    return A_B
+
+def main() :
+    (n,m,weight,connection) = get_input()
+    (A,B) = alg_R(n)
+    # (A,B) = alg_S(n,m,weight,connection,(list([0]*(n+1)),list([1]*(n+1))))
+    (A,B) = alg_S(n,m,weight,connection,(A,B))
+    print(A)
+    print(B)
+    max_cut = 0
+    for i in range(1,n+1) :
+        i_set = A if A[i] == 1 else B
+        for j in connection[i] :
+            max_cut += weight[(i,j)] * (1 if i_set[j] != 1 else 0)
+    max_cut /= 2    # Because I look throught all nodes, I double count the edges, so divide by 2
+    print(max_cut)
+
+main()
